9a2b7a873f97f68543824af97ddc581d3f42a44f
Abraham <aofolu87@gmail.com>
2026-01-08 19:00:52 +0100

    fix: stable login redirect for free render

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,9 +1,8 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, login_required
 from app.models.user import User
 
 auth_bp = Blueprint("auth", __name__)
-
 
 @auth_bp.route("/login", methods=["GET", "POST"])
 def login():
@@ -13,7 +12,7 @@ def login():
 
         user = User.query.filter_by(username=username).first()
 
-        if user and user.check_password(password):
+        if user and user.password == password:
             login_user(user)
             return redirect(url_for("finance.list_payments"))
 
@@ -23,6 +22,7 @@ def login():
 
 
 @auth_bp.route("/logout")
+@login_required
 def logout():
     logout_user()
     return redirect(url_for("auth.login"))
