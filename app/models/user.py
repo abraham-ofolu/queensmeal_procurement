766e082ce5260b5f8a188c6ae3dd21d0ed5d2e8b
Abraham <aofolu87@gmail.com>
2026-01-09 01:10:09 +0100

    Fix User.check_password for login

--- app/models/user.py
+++ app/models/user.py
@@ -1,8 +1,10 @@
 from flask_login import UserMixin
-from app import db
+from werkzeug.security import check_password_hash
+
+from app.extensions import db
 
 
-class User(db.Model, UserMixin):
+class User(UserMixin, db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
@@ -10,5 +12,5 @@ class User(db.Model, UserMixin):
     password_hash = db.Column(db.String(255), nullable=False)
     role = db.Column(db.String(50), nullable=False)
 
-    def __repr__(self):
-        return f"<User {self.username}>"
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
