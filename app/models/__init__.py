f4d2b048d6eba3261745b6d54b74870d9b8bcad4
Abraham <aofolu87@gmail.com>
2026-01-11 04:25:46 +0100

    Remove invalid procurement model import

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,11 +1,9 @@
 from .user import User
 from .vendor import Vendor
 from .procurement_request import ProcurementRequest
-from .procurement import Procurement
 from .procurement_quotation import ProcurementQuotation
 from .payment import Payment
 from .approval import Approval
 from .audit import Audit
 from .audit_log import AuditLog
 from .document import Document
-
