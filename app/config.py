dc82532bcea36552bc89a902d0e9a50e098e33be
Abraham <aofolu87@gmail.com>
2026-01-17 11:31:58 +0100

    fix app config

--- app/config.py
+++ app/config.py
@@ -1,7 +1,11 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
+    SECRET_KEY = os.environ.get("SECRET_KEY", "change-me")
 
-    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = (
+        os.environ.get("SQLALCHEMY_DATABASE_URI")
+        or os.environ.get("DATABASE_URL")
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
